b2d2f25d4936266518c2512aaddcd88d6d330bbc
BobirM <boburm029@mailcom>
2022-06-05 21:54:34 +0500

    commit1

--- src/component/Head/Header.js
+++ src/component/Head/Header.js
@@ -1,60 +1,65 @@
 import React, { useState } from "react"
 import "./header.css"
 import logo from "../pic/logo.png"
+import menu from "../pic/icons8-menu-24.png"
+import canel from '../pic/icons8-close-24.png'
 
 const Header = () => {
-  // fixed Header
-  window.addEventListener("scroll", function () {
-    const header = document.querySelector(".header")
-    header.classList.toggle("active", window.scrollY > 100)
-  })
-  // Toogle Menu
-  const [Mobile, setMobile] = useState(false)
-  return (
-    <>
-      <header className='header'>
-        <div className='container d_flex'>
-          <div className='logo'>
-            <img src={logo} alt='' style={{width:'58px',height:'60px',borderRadius:'50%',objectFit:'cover'}}/>
-          </div>
+	// fixed Header
+	window.addEventListener("scroll", function () {
+		const header = document.querySelector(".header")
+		header.classList.toggle("active", window.scrollY > 50)
+	})
+	// Toogle Menu
+	const [Mobile, setMobile] = useState(false)
+	return (
+		<>
+			<header className='header'>
+				<div className='container d_flex'>
+					<div className='logo'>
+						<img src={logo} alt='' style={{ width: '58px', height: '60px', borderRadius: '50%', objectFit: 'cover' }} />
+					</div>
 
-          <div className='navlink'>
-            <ul className={Mobile ? "nav-links-mobile" : "link f_flex uppercase"} onClick={() => setMobile(false)}>
-              {/*<ul className='link f_flex uppercase {Mobile ? "nav-links-mobile" : "nav-links"} onClick={() => setMobile(false)}'>*/}
-              <li>
-                <a href='#home'>home</a>
-              </li>
-              <li>
-                <a href='#features'>hobbies</a>
-              </li>
-              <li>
-                <a href='#portfolio'>portfolio</a>
-              </li>
-              <li>
-                <a href='#resume'>resume</a>
-              </li>
-              {/* <li>
+					<div className='navlink'>
+						<ul className={Mobile ? "nav-links-mobile" : "link f_flex uppercase"} onClick={() => setMobile(false)}>
+							{/*<ul className='link f_flex uppercase {Mobile ? "nav-links-mobile" : "nav-links"} onClick={() => setMobile(false)}'>*/}
+							{/* <li>
+								<a href='#home'>home</a>
+							</li> */}
+							<li>
+								<a href='#features'>hobbies</a>
+							</li>
+							<li>
+								<a href='#portfolio'>portfolio</a>
+							</li>
+							<li>
+								<a href='#resume'>resume</a>
+							</li>
+							{/* <li>
                 <a href='#clients'>clients</a>
               </li>
               <li>
                 <a href='#blog'>blog</a>
               </li> */}
-              <li>
-                <a href='#contact'>contact</a>
-              </li>
-              <li>
-                <button className='home-btn'>BUY NOW</button>
-              </li>
-            </ul>
+							<li>
+								<a href='#contact'>contact</a>
+							</li>
+							<li>
+							<a href='#home' className="home-btn">home</a>
+							</li>
+						</ul>
 
-            <button className='toggle' onClick={() => setMobile(!Mobile)}>
-              {Mobile ? <i className='fas fa-times close home-btn'></i> : <i className='fas fa-bars open'>        </i>}
-            </button>
-          </div>
-        </div>
-      </header>
-    </>
-  )
+						<button className='toggle' onClick={() => setMobile(!Mobile)}>
+							{Mobile ?
+								<i className='fas fa-times close home-btn'><img src={canel} alt="" /></i> :
+								<i className='fas fa-bars open'><img src={menu} alt="" /></i>
+							}
+						</button>
+					</div>
+				</div>
+			</header>
+		</>
+	)
 }
 
 export default Header
